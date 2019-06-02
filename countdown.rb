7e900536b8064caa2eb0a32273d9537cdcf8ffdf
magical-flow-3186 <rregalado17@gmail.com>
2019-06-02 22:40:36 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
-
-def countdown
+number = 10
+def countdown(number)
+while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
 end
+"HAPPY NEW YEAR!"
+end
+
+number = 10
+def countdown_with_sleep(number)
+while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep(1)
+end
+"HAPPY NEW YEAR!"
+end
+
